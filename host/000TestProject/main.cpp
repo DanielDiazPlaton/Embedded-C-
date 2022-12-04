6af05a24c887ad43d902b1ada028e854527f37bd
platonengener96 <daniel_dp96@hotmail.com>
2022-12-04 15:28:12 -0600

    Add Folder SMTCube IDE, target and host in C++

---
+++ host/000TestProject/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 18 nov 2022
+ *      Author: USER
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello world!\n";
+	cout << "Press enter to continue...\n";
+
+	getchar();
+
+	return 0;
+}
